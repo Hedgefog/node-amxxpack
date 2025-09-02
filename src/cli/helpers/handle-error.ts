a8161f2ad7078920cb1d7928cfb60ccb95a40fa9
Hedgehog Fog <hedgehogfogtv@gmail.com>
2025-09-02 17:58:33 +0200

    fix error message for help command

--- src/cli/helpers/handle-error.ts
+++ src/cli/helpers/handle-error.ts
@@ -1,7 +1,23 @@
+import { CommanderError } from "commander";
 import logger from "../../logger/logger";
 
 export default function handleError(err: unknown) {
-  logger.error(err instanceof Error ? err.message : 'Unknown error');
+  if (err instanceof CommanderError) {
+    switch (err.code) {
+      case 'commander.help': {
+        logger.info('Use `amxxpack --help` to see all available commands');
+        break;
+      }
+      default: {
+        logger.error(err.message);
+        break;
+      }
+    }
+  } else if (err instanceof Error) {
+    logger.error(err.message);
+  } else {
+    logger.error('Unknown error');
+  }
 
   if  (process.env.NODE_ENV === 'development') {
     throw err;
