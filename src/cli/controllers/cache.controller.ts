1dd217f3a9daa71347aaf8c52136e0d8e9dd7dd7
Hedgehog Fog <hedgehogfogtv@gmail.com>
2025-09-05 18:18:55 +0200

    add download dir to cache size calculation

--- src/cli/controllers/cache.controller.ts
+++ src/cli/controllers/cache.controller.ts
@@ -1,15 +1,25 @@
 import fs from 'fs';
 
 import { config } from '@common';
+import path from 'path';
 
 export default class CacheController {
   public getCacheSize(): number {
-    try {
-      const { size } = fs.statSync(config.cacheFile);
-      return size;
-    } catch (_err) {
-      return 0;
+    let size = 0;
+
+    if (fs.existsSync(config.cacheFile)) {
+      size += fs.statSync(config.cacheFile).size;
     }
+
+    if (fs.existsSync(config.downloadDir)) {
+      const files = fs.readdirSync(config.downloadDir);
+
+      for (const file of files) {
+        size += fs.statSync(path.join(config.downloadDir, file)).size;
+      }
+    }
+
+    return size;
   }
 
   public clearCache(): void {
